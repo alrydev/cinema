a7c6a3f03768ec6908443218d3b3ea2bd12e8313
unknown <alriydev@gmail.com>
2023-02-27 19:02:02 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="d-flex justify-content-between">
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    <img src="../src/assets/logoNavbar.png" />
+    <div class="me-5">
+      <button type="button" class="btn text-white fw-bold me-3">Login</button>
+      <button class="btn btn-pink text-white fw-bold">Register</button>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
+  <!-- <div>
+
+  </div> -->
 </template>
 
 <style>
@@ -16,15 +24,20 @@
 }
 
 nav {
-  padding: 30px;
+  background-color: black;
+  padding: 1rem;
 }
 
-nav a {
+/* nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+} */
+
+.btn-pink {
+  background-color: #CD2E71 !important;
 }
 </style>
